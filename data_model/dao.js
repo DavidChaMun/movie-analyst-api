f6ed246a4fe06a6485fff5283da53511376346ab
David Chaverra <dachaverramu@unal.edu.co>
2019-08-12 16:40:10 -0500

    Moved the database access logic to a dao, for better readability and mantainability

---
+++ data_model/dao.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+const credentials = require("../config/credentials.json")
+const configuration = require("../config/config.json")
+
+const envirorment = process.env.NODE_ENV || "development";
+const config = configuration[envirorment];
+
+var connection = mysql.createConnection({
+    host     : process.env.DB_HOST || config.host,
+    user     : process.env.DB_USER || credentials.movie_db.username,
+    password : process.env.DB_PASS || credentials.movie_db.password,
+    database : process.env.DB_NAME || config.database
+  });
+
+  connection.connect();
+
+  //Queries the database
+function getMovies(callback) {    
+    connection.query("SELECT * FROM movie_db.movies",
+        function (err, rows) {
+            callback(err, rows); 
+        }
+    );    
+}
+
+module.exports = {
+    getMovies,
+}
--- server.js
+++ server.js
@@ -1,32 +1,12 @@
 // Get our dependencies
 var express = require('express');
 var app = express();
-var mysql = require("mysql");
-const credentials = require("./config/credentials.json")
+var dao = require('./data_model/dao')
 const configuration = require("./config/config.json")
+
 
 const envirorment = process.env.NODE_ENV || "development";
 const config = configuration[envirorment];
-console.log(config)
-
-
-//mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com'
-var connection = mysql.createConnection({
-  host     : process.env.DB_HOST || config.host,
-  user     : process.env.DB_USER || credentials.movie_db.username,
-  password : process.env.DB_PASS || credentials.movie_db.password,
-  database : process.env.DB_NAME || config.database
-});
-
-connection.connect();
-
-function getMovies(callback) {    
-    connection.query("SELECT * FROM movie_db.movies",
-        function (err, rows) {
-            callback(err, rows); 
-        }
-    );    
-}
 
 //Testing endpoint
 app.get('/', function(req, res){
@@ -36,7 +16,7 @@ app.get('/', function(req, res){
 
 // Implement the movies API endpoint
 app.get('/movies', function(req, res){
-  getMovies((err, rows) => {
+  dao.getMovies((err, rows) => {
     res.json(rows);
   })
 })
