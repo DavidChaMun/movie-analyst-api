4382884f06f68948af6c268a6d139166c360d033
David Chaverra <dachaverramu@unal.edu.co>
2019-08-12 12:13:55 -0500

    Changed database to a local database for testing

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 // Get our dependencies
 var express = require('express');
 var app = express();
-//var mysql = require("mysql");
-//var connection = mysql.createConnection({
-//  host     : process.env.DB_HOST || 'mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com',
-//  user     : process.env.DB_USER || 'applicationuser',
-//  password : process.env.DB_PASS || 'applicationuser',
-//  database : process.env.DB_NAME || 'movie_db'
-//});
+var mysql = require("mysql");
 
-//connection.connect();
 
-//function getMovies(callback) {    
+
+//mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com'
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST || 'localhost',
+  user     : process.env.DB_USER || 'applicationuser',
+  password : process.env.DB_PASS || 'applicationuser',
+  database : process.env.DB_NAME || 'movie_db'
+});
+
+connection.connect();
+
+// function getMovies(callback) {    
 //        connection.query("SELECT * FROM movie_db.movies",
 //            function (err, rows) {
 //                callback(err, rows); 
 //            }
 //        );    
-//}
+// }
 
 //Testing endpoint
 app.get('/', function(req, res){
