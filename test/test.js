537b528b0b82b7fe583aa371a2af151f16bdf94f
David Chaverra <dachaverramu@unal.edu.co>
2019-08-14 08:45:40 -0500

    Added tv4 for json schema validation, added movies endpoint test that checks for status and for schema

--- test/test.js
+++ test/test.js
@@ -5,6 +5,8 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
+let tv4 = require('tv4-node').tv4;
+let movieSchema = require("./json_schemas/movie.json");
 
 chai.use(chaiHttp);
 //Our parent block
@@ -22,6 +24,18 @@ describe('API', () => {
               done();
             });
       });
+  });
+
+  describe('/GET movies', () => {
+    it('it should GET a JSON reply with VALID schema', (done) => {
+      chai.request(server)
+          .get('/movies')
+          .end((err, res) => {
+              res.should.have.status(200);
+              chai.assert(tv4.validate(res, movieSchema) == true, "Movies response is not a valid schema");
+            done();
+          });
+    });
   });
 
 });
