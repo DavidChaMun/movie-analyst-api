dd486ea763fc1e5b11c45a5f03f3404c63a499e9
David <david.loc.ch@gmail.com>
2019-08-22 15:39:03 -0500

    Fixed deploy script error

--- deploy_api.sh
+++ deploy_api.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 docker build -t movie_analyst_api ./
 host=$(docker exec -it movie_db_c01 bash -c 'IP=$(hostname -i); echo $IP')
-docker run --name malyst_apic01 -e DB_HOST=host -d movie_analyst_api
+docker run --name malyst_apic01 -e DB_HOST=$host -d movie_analyst_api
 echo ""
 echo -n "Movie Anlyst API listening on "
 echo -n $(docker exec -it malyst_apic01 bash -c 'IP=$(hostname -i); echo $IP')
